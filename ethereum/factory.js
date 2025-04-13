5853df5a1df3e1184261295a665a743b4cc7726e
arafat <arafat@mail.com>
2025-04-13 15:09:16 +0800

    removed

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -9,14 +9,6 @@ const abi =
 
 const instance = new web3.eth.Contract(
   abi,
-  '0xc9fDd40f73F4f74ff95756F08F50f81c55764084'
+  'Your Contract Address' //
 );
-
-//process.env.deployed_contract_address // Replace with the deployed contract address
-
-//1 - 0x49F16b275e4941624f2CCeF09097765f3B0dcA84 // with data
-//2 - 0x61B6E0f79F54c6606f54b1386E499E2b2e215bcc // with data
-//3 - 0x70C44d7d18E95CE12aD6468313544805D130dd7d // with campaign name but name not coming
-//4 - 0x19D45352549664fdD92878291CfC1909239b3565 // with campaign name
-
 export default instance;
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,31 +1,3 @@
-//Metamask
-
-// import Web3 from 'web3';
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-
-// let web3;
-
-// // Check if we're running in a browser or on the server
-// if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
-//   // We're in the browser and MetaMask is available
-//   web3 = new Web3(window.ethereum);
-
-//   // Request account access if needed
-//   window.ethereum.request({ method: 'eth_requestAccounts' }).catch((error) => {
-//     console.error('Error connecting to MetaMask:', error);
-//   });
-// } else {
-//   // We're on the server OR the user doesn't have MetaMask
-//   // Use our own provider with environment variables for security
-//   const provider = new HDWalletProvider(
-//     'battle promote scan joy lift spike remove identify erupt pet junior birth',
-//     'https://sepolia.infura.io/v3/09e89fac4db649a397524b4d6f80434a'
-//   );
-//   web3 = new Web3(provider);
-// }
-
-// export default web3;
-
 // direct HDWalletProvider
 import Web3 from 'web3';
 const HDWalletProvider = require('@truffle/hdwallet-provider');
@@ -33,8 +5,8 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 let web3;
 
 const provider = new HDWalletProvider(
-  'battle promote scan joy lift spike remove identify erupt pet junior birth',
-  'https://sepolia.infura.io/v3/09e89fac4db649a397524b4d6f80434a'
+  '', // Your mnemonic
+  '' // Your Infura endpoint
 );
 web3 = new Web3(provider);
 // }
